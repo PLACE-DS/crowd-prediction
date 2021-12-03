4c34485568dc15eca9a1164c99f34fc1603460d8
aidenta99 <taanh1999gha@gmail.com>
2021-12-03 01:15:57 +0100

    add simple model to generate forecasts for front-end development

--- data_loading.py
+++ data_loading.py
@@ -2,3 +2,13 @@ import pandas as pd
 
 df = pd.read_csv("data/sample.csv", index_col="Unnamed: 0")
 crowd_sample = df.iloc[:, 0:4]
+
+crowd_sample["Datetime"] = pd.to_datetime(crowd_sample['Time'])
+crowd_sample.set_index(["Datetime"], inplace=True)
+crowd_sample = crowd_sample.drop(["Time"], axis=1)
+
+# CMSA-GAWW-22
+crowd_sample["CMSA-GAWW-22 Kloveniersburgwal"].to_csv("data/CMSA-GAWW-22-Kloveniersburgwal.csv")
+sensor1 = pd.read_csv("data/CMSA-GAWW-22-Kloveniersburgwal.csv")
+sensor1["Datetime"] = pd.to_datetime(sensor1['Datetime'])
+sensor1 = sensor1.rename(columns={"CMSA-GAWW-22 Kloveniersburgwal": "count"})
--- simple_model_building.py
+++ simple_model_building.py
@@ -1,1 +1,27 @@
-# Crowd prediction based on simple ARIMA model, using sample data
+# Crowd prediction based on simple ARMA model, using sample data
+
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.arima_model import ARIMA
+import warnings
+from data_loading import sensor1
+
+# Specify to ignore warning messages
+warnings.filterwarnings("ignore")
+
+# Training set
+train = sensor1[-100:]
+
+# Fit ARIMA
+arima_fit = ARIMA(train["count"].values, order=(1,0,1)).fit()
+# Forecast the next 100 datetime points
+fc, se, conf  = arima_fit.forecast(100, alpha=0.05)  # 95% conf
+
+# Create a df with forecast intervals of the next 100 datetime points
+dt = [train.iloc[-1:]["Datetime"].values[0] + np.timedelta64(15*i,'m') for i in range(100)]
+forecasts = pd.DataFrame({"datetime": dt})
+forecasts["forecast"] = fc 
+forecasts["lower_bound"] = conf[:, 0]
+forecasts["upper_bound"] = conf[:, 1]
+
+forecasts.to_csv("sensor1_forecast.csv")
