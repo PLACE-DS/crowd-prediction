74ed2a138924600c98ba623ef5bd7f787753fc28
emiel <emiel@elnino.tech>
2021-12-05 23:56:32 +0100

    request now in session :clock:

--- ingestion/cov_ingestion.py
+++ ingestion/cov_ingestion.py
@@ -24,26 +24,27 @@ today = datetime.date.today().strftime("%Y-%m-%d")
 @click.option('--start_date', default=sensor_start, help='starting date as YYYY-MM-DD (default 2020-08-32)')
 @click.option('--end_date', default=today, help='end date as YYYY-MM-DD (default today)')
 def cov_ingestion(country, start_date, end_date):
-    request = f"https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/{start_date}/{end_date}/"
+    url = f"https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/{start_date}/{end_date}/"
 
-    response = requests.get(request)
+    with requests.Session() as s:
+        download = s.get(url, stream=True)
 
-    json = response.json()
+        json = download.json()
 
-    rows = []
-    for date in json["data"]:
-        rows.append(json["data"][date][country])
+        rows = []
+        for date in json["data"]:
+            rows.append(json["data"][date][country])
 
-    df = pd.DataFrame(rows)
+        df = pd.DataFrame(rows)
 
-    outname = f'cov_{country.lower()}_raw.csv'
-    outdir = os.path.join('data', 'raw')
-    fullpath = os.path.join(outdir, outname)
+        outname = f'cov_{country.lower()}_raw.csv'
+        outdir = os.path.join('data', 'raw')
+        fullpath = os.path.join(outdir, outname)
 
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
 
-    df.to_csv(fullpath)
+        df.to_csv(fullpath)
 
 
 if __name__ == "__main__":
