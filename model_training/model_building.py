6dfedcdf15ffde18fbdd53c82477f5921ef92a21
aidenta99 <taanh1999gha@gmail.com>
2021-12-06 18:22:34 +0100

    remove data-loading file

--- model_training/model_building.py
+++ model_training/model_building.py
@@ -11,8 +11,23 @@ from data_loading import kloveniersburgwal
 warnings.filterwarnings("ignore")
 
 # Prepare the data
-data = kloveniersburgwal
+df = pd.read_csv("data/sample.csv", index_col="Unnamed: 0")
+crowd_sample = df.iloc[:, 0:4]
 
+crowd_sample["Datetime"] = pd.to_datetime(crowd_sample['Time'])
+crowd_sample.set_index(["Datetime"], inplace=True)
+crowd_sample = crowd_sample.drop(["Time"], axis=1)
+
+# CMSA-GAWW-22
+crowd_sample["CMSA-GAWW-22 Kloveniersburgwal"].to_csv("data/CMSA-GAWW-22-Kloveniersburgwal.csv")
+kloveniersburgwal = pd.read_csv("data/CMSA-GAWW-22-Kloveniersburgwal.csv")
+kloveniersburgwal["objectnummer"] = "CMSA-GAWW-22"
+kloveniersburgwal["location"] = "Kloveniersburgwal"
+kloveniersburgwal["Datetime"] = pd.to_datetime(kloveniersburgwal['Datetime'])
+kloveniersburgwal = kloveniersburgwal.rename(columns={"CMSA-GAWW-22 Kloveniersburgwal": "count", "Datetime": "datetime"})
+kloveniersburgwal = kloveniersburgwal.reindex(columns=["objectnummer", "location", "datetime", "count"])
+
+data = kloveniersburgwal
 
 # Training-test set split
 # split a univariate dataset into train/test sets
