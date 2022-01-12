2f481774dc9f1a7b425abd7a6b77e16aa2c26021
aidenta99 <tangocanh99hnams@gmail.com>
2022-01-12 17:27:30 +0100

    drop unncessary cols

---
+++ model_building/model_building.py
@@ -0,0 +1,95 @@
+# Crowd prediction based on simple ARMA model and XGBoost, using sample data
+
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.arima_model import ARIMA
+from xgboost import XGBRegressor
+import warnings
+from data_loading import kloveniersburgwal
+
+# Specify to ignore warning messages
+warnings.filterwarnings("ignore")
+
+# Prepare the data
+df = pd.read_csv("data/sample.csv", index_col="Unnamed: 0")
+crowd_sample = df.iloc[:, 0:4]
+
+crowd_sample["Datetime"] = pd.to_datetime(crowd_sample['Time'])
+crowd_sample.set_index(["Datetime"], inplace=True)
+crowd_sample = crowd_sample.drop(["Time"], axis=1)
+
+# CMSA-GAWW-22
+crowd_sample["CMSA-GAWW-22 Kloveniersburgwal"].to_csv("data/CMSA-GAWW-22-Kloveniersburgwal.csv")
+kloveniersburgwal = pd.read_csv("data/CMSA-GAWW-22-Kloveniersburgwal.csv")
+kloveniersburgwal["objectnummer"] = "CMSA-GAWW-22"
+kloveniersburgwal["location"] = "Kloveniersburgwal"
+kloveniersburgwal["Datetime"] = pd.to_datetime(kloveniersburgwal['Datetime'])
+kloveniersburgwal = kloveniersburgwal.rename(columns={"CMSA-GAWW-22 Kloveniersburgwal": "count", "Datetime": "datetime"})
+kloveniersburgwal = kloveniersburgwal.reindex(columns=["objectnummer", "location", "datetime", "count"])
+
+data = kloveniersburgwal
+
+# Training-test set split
+# split a univariate dataset into train/test sets
+def train_test_split(data, n_test):
+    return data[:n_test], data[n_test:]
+
+train, test = train_test_split(data, 400)
+
+# Fit ARIMA
+arima_fit = ARIMA(train["count"].values, order=(6, 0, 3)).fit()
+# Forecast the next 100 datetime points
+fc, se, conf = arima_fit.forecast(100, alpha=0.05)  # 95% conf
+
+# Create a df with forecast intervals of the next 100 datetime points
+
+# Get the following datetime (after the last point of training set)
+dt = [train.iloc[-1:]["datetime"].values[0] + np.timedelta64(15 * i, 'm') for i in range(100)]
+
+# Extract the forecasts
+forecasts = pd.DataFrame({"datetime": dt})
+forecasts["forecast"] = fc
+forecasts["lower_bound"] = conf[:, 0]
+forecasts["upper_bound"] = conf[:, 1]
+
+# Result file - just to test the workflow
+
+# Future work: extract objectnummer, location, threshold into forecast file 
+forecasts["objectnummer"] = "CMSA-GAWW-22"
+forecasts["location"] = "Kloveniersburgwal"
+forecasts["threshold"] = 182.5  # wrong number
+forecasts = forecasts.reindex(
+    columns=["objectnummer", "location", "datetime", "forecast", "lower_bound", "upper_bound", "threshold"])
+
+forecasts.to_csv("result/sensor1_forecast.csv")
+
+
+# XGBoost
+
+# Prepare features
+def create_time_features(df, label=None):
+    """
+    Creates time series features from datetime index
+    """
+    df['hour'] = df['datetime'].dt.hour
+    df['dayofweek'] = df['datetime'].dt.dayofweek
+    df['quarter'] = df['datetime'].dt.quarter
+    df['month'] = df['datetime'].dt.month
+    df['year'] = df['datetime'].dt.year
+    df['dayofyear'] = df['datetime'].dt.dayofyear
+    df['dayofmonth'] = df['datetime'].dt.day
+    df['weekofyear'] = df['datetime'].dt.weekofyear
+
+    X = df[['hour', 'dayofweek', 'quarter', 'month', 'year',
+            'dayofyear', 'dayofmonth', 'weekofyear']]
+    if label:
+        y = df[label]
+        return X, y
+    return X
+
+y_train = train[["count"]]
+X_train = train.drop(["count"], axis=1)
+X_train = create_time_features(X_train)
+
+xgboost = XGBRegressor(n_estimators=1000)
+xgboost_fit = xgboost.fit(X_train, y_train)
