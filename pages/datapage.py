d08694403995718a9a0c69e7a8f947faf77c3be3
laurahilhorst <xlaurahilhorst@live.nl>
2022-01-19 16:09:52 +0100

    add date feature to datapage.py

--- pages/datapage.py
+++ pages/datapage.py
@@ -23,7 +23,12 @@ def app():
     cmsa_per_day = cmsa_all.resample('D', on='datetime').sum()
     sensors = cmsa_all.columns[1:4]
     columns = cmsa_all.columns[4:]
+    # knmi_columns = [19:]
+    # gvb_columns = [7:10]
+    # tourism_columns = cmsa_all.columns[11:15]
+    # covid_columns = [16:18]
     st.title('Data and Code')
+    st.markdown('Use the date selector and column checkboxes to export the data of your choice. Our')
 
     start_dt = st.date_input('Start date',
         value=cmsa_per_day.index.min(),
@@ -35,25 +40,22 @@ def app():
         min_value = cmsa_per_day.index.min(),
         max_value = cmsa_per_day.index.max())
 
+    st.write(start_dt,end_dt)
+
     st.header('Select sensors')
     check_sensors = [st.checkbox(i, key=i) for i in sensors]
     st.header('Select metadata')
     check_columns = [st.checkbox(i, key=i) for i in columns]
     checked_sensors = [stock for stock, checked in zip(sensors, check_sensors) if checked]
     checked_columns = [stock for stock, checked in zip(columns, check_columns) if checked]
-    st.write(checked_sensors, checked_columns)
-    # st.checkbox('KNMI')
-    # st.checkbox('GVB')s
-    # st.checkbox('Covid')
-    # st.checkbox('Tourism')
 
     def convert_df(df):
         return df.to_csv()
 
     downloadcolumns = ['datetime'] + list(checked_sensors) + list(checked_columns)
-    st.write(downloadcolumns)
+   
 
     downloadcsv = convert_df(cmsa_all[downloadcolumns])
 
-    st.download_button(label = 'download .csv', data=downloadcsv, mime='text/csv')
+    st.download_button(label = 'download .csv', data=downloadcsv[(downloadcsv['datetime'] > start_dt) & (downloadcsv['datetime'] < end_dt)], mime='text/csv')
 
