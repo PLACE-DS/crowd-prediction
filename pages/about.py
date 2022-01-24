6a4b99b60bb68dc0c69fac475d5e85422aac3fa5
Paula Sorolla <55831646+paula-sorolla@users.noreply.github.com>
2022-01-24 12:32:35 +0100

    Added members in About page

--- pages/about.py
+++ pages/about.py
@@ -9,13 +9,28 @@ def app():
         existing prediction model for crowdedness based on CMSA passenger data and improving it.' )
 
     st.header('About the team')
-    st.markdown('Team members:  \
-        - Chieling Yueh  \
-        - Emiel Steegh  \
-        - Paula Sorolla  \
-        - Aiden Ta  \
-        - Laura Hilhorst')
 
+    w2, w3, w4, w5, w6 = st.columns(5)
+
+    w4.image("memoji/chieling.png")
+    w4.markdown("<h3 style='text-align: center;'>Chieling Yue</h3>", unsafe_allow_html=True)
+    w4.markdown("<p style='text-align: center;'>chielingyueh@gmail.com</p>", unsafe_allow_html=True)
+
+    w5.image("memoji/emiel.png")
+    w5.markdown("<h3 style='text-align: center;'>Emiel Steegh</h3>", unsafe_allow_html=True)
+    w5.markdown("<p style='text-align: center;'>emielSteegh@gmail.com</p>", unsafe_allow_html=True)
+
+    w6.image("memoji/paula.png")
+    w6.markdown("<h3 style='text-align: center;'>Paula Sorolla</h3>", unsafe_allow_html=True)
+    w6.markdown("<p style='text-align: center;'>paula.sorolla@gmail.com</p>", unsafe_allow_html=True)
+
+    w3.image("memoji/aiden.png")
+    w3.markdown("<h3 style='text-align: center;'>Aiden Ta</h3>", unsafe_allow_html=True)
+    w3.markdown("<p style='text-align: center;'>taanh1999GHA@gmail.com</p>", unsafe_allow_html=True)
+
+    w2.image("memoji/laura.png")
+    w2.markdown("<h3 style='text-align: center;'>Laura Hilhorst</h3>", unsafe_allow_html=True)
+    w2.markdown("<p style='text-align: center;'>laura.hilhorst2@gmail.com</p>", unsafe_allow_html=True)
 
 
 
